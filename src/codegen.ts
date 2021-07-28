e6bd36d1c1eba8e77493d3c154e52d5e15986aa3
gosho-kazuya <ketsume0211@gmail.com>
2021-07-29 00:56:58 +0900

    add missing file

---
+++ src/codegen.ts
@@ -0,0 +1,34 @@
+export const baseSchemaCode = `// eslint-disable
+
+type Maybe<T> = T | null | undefined`
+
+export function gqlTypeToTsType(gqlType: string) {
+  switch (gqlType) {
+    case 'String!':
+      return 'string'
+    case 'String':
+      return 'Maybe<string>'
+    case 'ID!':
+      return 'string' // TODO: specify IDType
+    case 'ID':
+      return 'Maybe<string>'
+    default:
+      const matchNonNullArray = gqlType.match(/\[(.+)\!]!$/)
+      if (matchNonNullArray) {
+        return `${matchNonNullArray[1]}[]`
+      }
+      const matchNullableArray = gqlType.match(/\[(.+)\!]$/)
+      if (matchNullableArray) {
+        return `Maybe<${matchNullableArray[1]}[]>`
+      }
+      const maybeMatchNullableArray = gqlType.match(/\[(.+)\]$/)
+      if (maybeMatchNullableArray) {
+        return `Maybe<Maybe<${maybeMatchNullableArray[1]}>[]>`
+      }
+      const nonNullableType = gqlType.match(/(.+)!$/)
+      if (nonNullableType) {
+        return `${nonNullableType[1]}`
+      }
+      return `Maybe<${gqlType}>`
+  }
+}
